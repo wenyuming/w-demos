737da0e5d08291e03e96ca228324101250b81273
温宇鸣 <591339209@qq.com>
2019-01-09 10:59:53 +0800

    更新图片下载

--- src/demos/downloadqrcode.vue
+++ src/demos/downloadqrcode.vue
@@ -38,10 +38,23 @@
       downloadQrcode () {
         var canvasData = document.getElementById('canvas');
         console.dir(canvasData, 'canvasData')
-        var a = document.createElement("a");
-        a.href = canvasData.toDataURL();;
-        a.download = "drcQrcode";
-        a.click()
+        // var a = document.createElement("a");
+        // a.href = canvasData.toDataURL();;
+        // a.download = "drcQrcode";
+        // a.click()
+        let url = canvasData.toDataURL()
+        var savaFile=function(data,filename)
+        {
+            var save_link=document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+            save_link.href=data;
+            save_link.download=filename;
+            var event=document.createEvent('MouseEvents');
+            event.initMouseEvent('click',true,false,window,0,0,0,0,0,false,false,false,false,0,null);
+            save_link.dispatchEvent(event);
+        }
+        var filename=''+new Date().getDate();  
+        //注意咯 由于图片下载的比较少 就直接用当前几号做的图片名字
+        savaFile(url,filename);
       }
     },
     mounted () {
